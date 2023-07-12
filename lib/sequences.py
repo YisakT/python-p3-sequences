8f9f3420d5c4a6d70f0e0a3e11066cc4e55d7dd8
yisak <yisakbruck23@gmail.com>
2023-07-12 17:20:45 -0400

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    fib_sequence = []
+
+    if length == 1:
+        fib_sequence.append(0)
+        print(fib_sequence)
+        return
+
+    if length <= 0:
+       
+        print(fib_sequence)
+        return
+
+    
+    fib_1 = 0
+    fib_2 = 1
+
+    
+    fib_sequence.append(fib_1)
+    fib_sequence.append(fib_2)
+
+    
+    for i in range(2, length):
+        next_fib = fib_1 + fib_2
+        fib_sequence.append(next_fib)
+        fib_1 = fib_2
+        fib_2 = next_fib
+
+    print(fib_sequence)
